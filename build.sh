bc230436bea9e1592a5f11ac2bdf3861a9c282b8
tarunreddykalluri <tarun9553164245@gmail.com>
2025-12-11 10:31:10 +0530

    Prepared for render

--- build.sh
+++ build.sh
@@ -1,14 +1,7 @@
-# #!/usr/bin/env bash
-# # Exit on error
-# set -o errexit
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
 
-# pip install -r requirements.txt
-# python manage.py collectstatic --no-input
-# python manage.py migrate
-
-dj-database-url
-Django
-gunicorn
-mysqlclient
-psycopg2-binary
-whitenoise
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
