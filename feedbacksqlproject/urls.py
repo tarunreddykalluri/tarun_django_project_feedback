df00ee668296a5eeaa59bb778ba5be060e8b4bec
tarunreddykalluri <tarun9553164245@gmail.com>
2025-12-11 12:15:29 +0530

    last

--- feedbacksqlproject/urls.py
+++ feedbacksqlproject/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings             # <--- Import settings
+from django.conf.urls.static import static   # <--- Import static helper
 from myapp.views import feedbackview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('feed/', feedbackview),
 ]
+
+# This tells Django: "Please verify and serve the static files (CSS) correctly"
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
